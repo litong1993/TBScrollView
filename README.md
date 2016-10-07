99461cc477ae05fe954945852fa4e1c263d7941d
李童 <litong@litongdeMacBook-Pro.local>
2016-10-07 12:20:31 +0800

    添加说明文档

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # TBScrollView
+可循环自动滚动的ScrollViwe
+# 使用方法
+
+```swift
+override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    //初始化
+    scroll = TBScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 200),delegate:self)
+    //设置自动滚动的时间
+    scroll?.interval = 1
+    self.view.addSubview(scroll!)
+
+}
+```
+
+# 代理方法
+```swift
+//设置内容数量
+func numberInScroll() -> Int {
+    return models.count
+}
+//设置内容图片
+func itemOfIndex(index: Int, imageView: UIImageView) {
+    let item = models[index]
+    imageView.image = UIImage(named: item.imageUrl)
+}
+//点击事件
+func didSelItem(index: Int) {
+    print(index)
+}
+```
--- TBScrollView/TBScrollView/ViewController.swift
+++ TBScrollView/TBScrollView/ViewController.swift
@@ -22,7 +22,9 @@ class ViewController: UIViewController,TBScrollViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //初始化
         scroll = TBScrollView(frame: CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: 200),delegate:self)
+        //设置自动滚动的时间
         scroll?.interval = 1
         self.view.addSubview(scroll!)
         
@@ -33,13 +35,16 @@ class ViewController: UIViewController,TBScrollViewDelegate {
         // Dispose of any resources that can be recreated.
     }
     let models = [TBRecommend(img: "0", url: ""),TBRecommend(img: "1", url: ""),TBRecommend(img: "2", url: ""),TBRecommend(img: "0", url: ""),TBRecommend(img: "1", url: ""),TBRecommend(img: "2", url: ""),TBRecommend(img: "0", url: "")]
+    //设置内容数量
     func numberInScroll() -> Int {
         return models.count
     }
+    //设置内容图片
     func itemOfIndex(index: Int, imageView: UIImageView) {
         let item = models[index]
         imageView.image = UIImage(named: item.imageUrl)
     }
+    //点击事件
     func didSelItem(index: Int) {
         print(index)
     }
